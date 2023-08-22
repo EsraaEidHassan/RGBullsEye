63b9e45b6d6bbb3da2eef15088d634203cffc327
EsraaEidHassan <esraa.eidhassan@gmail.com>
2023-08-22 12:06:29 +0300

    add hit me button functioanlity and make the color circular

--- RGBullsEye/RGBullsEye/ContentView.swift
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -10,17 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State var game : Game = Game()
     @State var guess : RGB
+    @State var showScore = false
     var body: some View {
         VStack {
-            Color(rgbStruct: game.target)
-            Text("R: ??? G: ??? B: ???").padding()
-            Color(rgbStruct: guess)
+            Circle()
+                .fill(Color(rgbStruct: game.target))
+            if !showScore {
+                Text("R: ??? G: ??? B: ???").padding()
+            } else {
+                Text(game.target.intString()).padding()
+            }
+            Circle()
+                .fill(Color(rgbStruct: guess))
             Text(guess.intString()).padding()
             ColorSlider(value: $guess.red, trackColor: .red)
             ColorSlider(value: $guess.green, trackColor: .green)
             ColorSlider(value: $guess.blue, trackColor: .blue)
-            Button("textt") {
+            Button("Hit Me!") {
+                showScore = true
+                game.check(guess: guess)
                 
+            }.alert(isPresented: $showScore) {
+                Alert(title: Text("Your Score"),
+                      message: Text(String(game.scoreRound)),
+                      dismissButton: .default(Text("OK")){
+                    game.startNewRound()
+                    guess = RGB()
+                }
+                )
             }
             
             
